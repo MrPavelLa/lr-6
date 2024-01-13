5c171e6d4f104265e8481d72ae3dfa3e0f640485
MrPavelLa <ki.stonerr@gmail.com>
2024-01-13 10:33:35 +0300

    first commit

---
+++ src/components/mains/Replenishment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Replenishment = () => {
+
+  return (    
+    <>
+    <h1>ДО<span style={{ fontWeight: 'bold', color: 'red'}}>ДЕ</span>ЛАЮ КОГДА-НИБУДЬ</h1>
+    <p>надеюсь никогда</p>
+    </>
+  );
+};
+
+export default Replenishment;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -15,3 +18,30 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { Provider } from 'react-redux';
+// import store from './store'
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
